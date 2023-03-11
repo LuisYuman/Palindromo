1a07771fba15f2229857c2b3d126067a9cdc82d6
Luis Yuman <lyuman737@gmail.com>
2023-03-11 11:26:42 -0600

    Agregar archivos de proyecto.

---
+++ Palindromo/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Console.WriteLine("Ingrese una palabra: ");
+string palabra = Console.ReadLine();
+
+if(espalindromo(palabra))
+{
+    Console.WriteLine($"{palabra} Es un palindromo");
+
+}
+else
+{
+    Console.WriteLine($"{palabra} No es un palindromo");
+}
+
+bool espalindromo(string palabra)
+{
+    palabra= palabra.ToLower();
+
+    int longitud = palabra.Length;
+    for(int i = 0; i < longitud/2;i++ )
+    {
+        if (palabra[i] != palabra[longitud -i -1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
